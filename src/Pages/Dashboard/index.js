e47ae412c654086df2bceed7675a083fb436996d
FabianoPereiraOne <68227160+FabianoPereiraOne@users.noreply.github.com>
2021-05-18 22:02:51 -0300

    finish style table dashboard

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,12 +1,12 @@
 import Header from '../../Components/Header'
 import './dashboard.css'
 import Title from '../../Components/Title'
-import { FiMessageSquare, FiPlus } from 'react-icons/fi'
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
 function Dashboard(){
-    const [ chamados, setChamados ] = useState([])
+    const [ chamados, setChamados ] = useState(["j"])
     return(
         <div className="contentDash">
             <Header/>
@@ -15,23 +15,109 @@ function Dashboard(){
                 <Title name="Atendimentos">
                     <FiMessageSquare/>
                 </Title>
-                <section className="section_chamados">
-                    {
-                        chamados.length === 0 ?
-                        (
-                            <>
-                                <span>Nenhum chamado resgistrado...</span>
+                {
+                    chamados.length === 0 ?
+                    (
+                        <section chamados="section_chamados">
+                            <span>Nenhum chamado resgistrado...</span>
+                            <Link to="/new">
+                                <FiPlus/>
+                                Novo chamado
+                            </Link>
+                        </section>
+                    ):
+                    (
+                        <>
+                            <section className="section_newChamados">
                                 <Link to="/new">
                                     <FiPlus/>
                                     Novo chamado
                                 </Link>
-                            </>
-                        ):
-                        (
-                            <h1>Ja tem chamados</h1>
-                        )
-                    }
-                </section>
+                            </section>
+                            <table className="table_chamados">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Cliente</th>
+                                        <th scope="col">Assunto</th>
+                                        <th scope="col">Status</th>
+                                        <th scope="col">Cadastrado em</th>
+                                        <th scope="col"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td data-label="Cliente">Name Teste</td>
+                                        <td data-label="Assunto">Suporte</td>
+                                        <td data-label="Status" className="content_badge"><div data-situacao="aberto">Em aberto</div></td>
+                                        <td data-label="Cadastrado">12/05/2021</td>
+                                        <td data-label="" className="content_btn">
+                                            <div>
+                                                <button className="btn_search">
+                                                    <FiSearch/>
+                                                </button>
+                                                <button className="btn_edit">
+                                                    <FiEdit2/>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td data-label="Cliente">Name Teste</td>
+                                        <td data-label="Assunto">Suporte</td>
+                                        <td data-label="Status" className="content_badge"><div data-situacao="progress">Em progresso</div></td>
+                                        <td data-label="Cadastrado">12/05/2021</td>
+                                        <td data-label="" className="content_btn">
+                                            <div>
+                                                <button className="btn_search">
+                                                    <FiSearch/>
+                                                </button>
+                                                <button className="btn_edit">
+                                                    <FiEdit2/>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td data-label="Cliente">Name Teste</td>
+                                        <td data-label="Assunto">Suporte</td>
+                                        <td data-label="Status" className="content_badge"><div data-situacao="atendido">Atendido</div></td>
+                                        <td data-label="Cadastrado">12/05/2021</td>
+                                        <td data-label="" className="content_btn">
+                                            <div>
+                                                <button className="btn_search">
+                                                    <FiSearch/>
+                                                </button>
+                                                <button className="btn_edit">
+                                                    <FiEdit2/>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td data-label="Cliente">Name Teste</td>
+                                        <td data-label="Assunto">Suporte</td>
+                                        <td data-label="Status" className="content_badge"><div data-situacao="aberto">Em aberto</div></td>
+                                        <td data-label="Cadastrado">12/05/2021</td>
+                                        <td data-label="" className="content_btn">
+                                            <div>
+                                                <button className="btn_search">
+                                                    <FiSearch/>
+                                                </button>
+                                                <button className="btn_edit">
+                                                    <FiEdit2/>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                               
+                                </tbody>
+
+                            </table>
+                        
+                        
+                        </>
+                    )
+                }
             </div>
         </div>
     )
