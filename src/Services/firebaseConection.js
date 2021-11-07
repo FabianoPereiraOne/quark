13cc1dc48cedc9f75459e29599df1267fc909903
Fabiano Pereira <fabianop971@gmail.com>
2021-11-07 20:59:56 -0300

    Bug( Firebase ): Add datas

--- src/Services/firebaseConection.js
+++ src/Services/firebaseConection.js
@@ -3,13 +3,13 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/storage'
 
-let firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.API_ID
+const firebaseConfig = {
+  apiKey: "AIzaSyBHZqBtjwADORcX9Xb1hyhKBDblHJA4Rf4",
+  authDomain: "quark-cb3c2.firebaseapp.com",
+  projectId: "quark-cb3c2",
+  storageBucket: "quark-cb3c2.appspot.com",
+  messagingSenderId: "255513859802",
+  appId: "1:255513859802:web:f618effd01a5012055f7e3"
 };
 
 
